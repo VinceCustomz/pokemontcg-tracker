871649598a5a2f0e682c9615f43e9a3e3d30c4da
vincecustomz <vince.vuong@live.com>
2022-06-11 16:55:02 -0400

    User Login Successful

--- src/App.js
+++ src/App.js
@@ -1,45 +1,41 @@
-import './App.css';
-import React, {Component} from 'react'
-import AuthPage from './pages/AuthPage/AuthPage'
-import MainPage from './components/MainPage/MainPage';
+import "./App.css";
+import React, { Component } from 'react'
+import AuthPage from "./pages/AuthPage/AuthPage";
+import MainPage from "./components/MainPage/MainPage";
 
 export default class App extends Component {
+  
   state = {
-    user: null
-  }
+    user: null,
+  };
 
   setUserInState = (incomingUserData) => {
-    this.setState({ user: incomingUserData})
-  }
-  
+    this.setState({ user: incomingUserData });
+  };
+
   componentDidMount() {
-    let token = localStorage.getItem('token')
+    let token = localStorage.getItem("token");
 
     if (token) {
-      const payload = JSON.parse(atob(token.split('.')[1]))
-      if (payload.exp < Date.now() /1000) {
-
-        localStorage.removeItem() 
-        token = null
+      const payload = JSON.parse(atob(token.split(".")[1]));
+      if (payload.exp < Date.now() / 1000) {
+        localStorage.removeItem();
+        token = null;
       } else {
-        this.setState({ user: payload.user })
+        this.setState({ user: payload.user });
       }
     }
-
   }
 
   render() {
     return (
-      <div className="App">
-      {this.state.user ?
-        <MainPage setUserInState={this.setUserInState}/>
-    :
-        <AuthPage setUserInState={this.setUserInState} />
-    }
-
-    </div>
+      <main className="App">
+        {this.state.user ? (
+          <MainPage setUserInState={this.setUserInState} />
+        ) : (
+          <AuthPage setUserInState={this.setUserInState} />
+        )}
+      </main>
     );
   }
-
 }
-
