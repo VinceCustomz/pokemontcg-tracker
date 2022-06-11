49f5b2a5d63b884f82513642fe540c2749fb4df0
vincecustomz <vince.vuong@live.com>
2022-06-11 05:23:46 -0400

    Cant seem to sign up and login

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,46 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class LoginForm extends Component {
-    state = {
-        email: '',
-        password: '',
-        error: '',
+  state = {
+    email: "",
+    password: "",
+    error: "",
+  };
+
+  handleChange = (evt) => {
+    this.setState({
+      [evt.target.name]: evt.target.value,
+      error: "",
+    });
+  };
+
+  handleSubmit = async (evt) => {
+    evt.preventDefault();
+    try {
+      const fetchResponse = await fetch("/api/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email: this.state.email,
+          password: this.state.password,
+        }),
+      });
+
+      if (!fetchResponse.ok) throw new Error("Fetch failed - Bad request");
+
+      let token = await fetchResponse.json();
+      localStorage.setItem("token", token);
+
+      const userDoc = JSON.parse(atob(token.split(".")[1])).user;
+      this.props.setUserInState(userDoc);
+    } catch (err) {
+      console.log("SignupForm error", err);
+      this.setState({ error: "Login/Sign Up Failed - Try Again" });
     }
-    
-    handleChange = (evt) => {
-        this.setState({
-          [evt.target.name]: evt.target.value,
-          error: "",
-        });
-      };
-    
-      handleSubmit = async (evt) => {
-        evt.preventDefault();
-        try {
-          const fetchResponse = await fetch('/api/users/login', {
-            method: 'POST',
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ 
-              email: this.state.email, 
-              password: this.state.password,
-             })
-          })
+  };
 
-          if (!fetchResponse.ok) throw new Error('Fetch failed - Bad request')
-    
-          let token = await fetchResponse.json()
-          localStorage.setItem('token', token);
-    
-          const userDoc = JSON.parse(atob(token.split('.')[1])).user;
-          this.props.setUserInState(userDoc)
-    
-        } catch (err) {
-          console.log("SignupForm error", err);
-          this.setState({ error: "Sign Up Failed - Try Again" });
-        }
-      };
-
-    render() {
+  render() {
     return (
       <div>
         <div className="form-container" onSubmit={this.handleSubmit}>
@@ -66,6 +65,6 @@ export default class LoginForm extends Component {
         </div>
         <p className="error-message">&nbsp;{this.state.error}</p>
       </div>
-    )
+    );
   }
 }
