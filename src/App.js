c2ca3ad2ec3f8f9261e6a53a6fbdfa0a38e71c3c
vincecustomz <vince.vuong@live.com>
2022-06-11 05:04:09 -0400

    Auth Almost added in

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import './App.css';
+import React, {Component} from 'react'
+import AuthPage from './pages/AuthPage/AuthPage'
+import MainPage from './components/MainPage/MainPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Hi Hi this is Pokemon TCG Tracker
-      </header>
+export default class App extends Component {
+  state = {
+    user: null
+  }
+
+  setUserInState = (incomingUserData) => {
+    this.setState({ user: incomingUserData})
+  }
+  
+  componentDidMount() {
+    let token = localStorage.getItem('token')
+
+    if (token) {
+      const payload = JSON.parse(atob(token.split('.')[1]))
+      if (payload.exp < Date.now() /1000) {
+
+        localStorage.removeItem() 
+        token = null
+      } else {
+        this.setState({ user: payload.user })
+      }
+    }
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+      {this.state.user ?
+        <MainPage setUserInState={this.setUserInState}/>
+    :
+        <AuthPage setUserInState={this.setUserInState} />
+    }
+
     </div>
-  );
+    );
+  }
+
 }
 
-export default App;
