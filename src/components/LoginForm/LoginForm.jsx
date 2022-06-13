e6eaeb786fbe521d2b2118dcb1c9c51053ef0cdf
zainamirza <zain_amir@outlook.com>
2022-06-13 10:07:55 -0400

    login styling

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -43,28 +43,41 @@ export default class SignUpForm extends Component {
     return (
       <div>
         <div className="form-container" onSubmit={this.handleSubmit}>
-          <form autoComplete="off">
-            <label>Email</label>
+          <form autoComplete="off" className="sm:w-2/3 w-full px-4 lg:px-0 mx-auto">
+            {/* <label>Email</label> */}
             <input
+              className="block w-full p-4 text-lg rounded-sm bg-gray-100 mt-12 mb-12"
               type="text"
               name="email"
               value={this.state.email}
               onChange={this.handleChange}
+              placeholder="Email"
               required
             />
-            <label>Password</label>
+            {/* <label>Password</label> */}
             <input
+              className="block w-full p-4 text-lg rounded-sm bg-gray-100 mt-12 mb-12"
               type="password"
               name="password"
               value={this.state.password}
               onChange={this.handleChange}
+              placeholder="Password"
               required
             />
-            <button type="submit">LOG IN</button>
+            <button type="submit" 
+            className="uppercase block mt-12 mb-12 w-full p-4 text-lg rounded-full bg-indigo-500 hover:bg-indigo-600 focus:outline-none">
+              LOG IN
+              </button>
+
           </form>
         </div>
         <p className="error-message">&nbsp;{this.state.error}</p>
       </div>
+
+
+
+
+      
     );
   }
 }
