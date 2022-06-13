4d2e47d9a575f0572e0d83277898f5d0213d142f
zainamirza <zain_amir@outlook.com>
2022-06-13 11:42:33 -0400

    trying to style again

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -33,12 +33,34 @@ class LoginForm extends React.Component {
     render() {
         return (
             <div className='login-container' onSubmit={this.handleSubmit}>
-                <form autoComplete="off">
-                    <label>Email: </label>
-                    <input type='text' name='email' value={this.state.email} onChange={this.handleChange} required />
-                    <label>Password: </label>
-                    <input type='password' name='password' value={this.state.password} onChange={this.handleChange} required />
-                    <button type='submit'>Login</button>
+                <form autoComplete="off" className="sm:w-2/3 w-full px-4 lg:px-0 mx-auto">
+                    {/* <label>Email: </label> */}
+                    
+                    <input 
+                    className="block w-full p-4 text-lg rounded-sm bg-gray-100 mt-12 mb-12"
+                    placeholder="Email"
+                    type='text' 
+                    name='email' 
+                    value={this.state.email} 
+                    onChange={this.handleChange} 
+                    required />
+                    
+                    {/* <label>Password: </label> */}
+                    
+                    <input 
+                    className="block w-full p-4 text-lg rounded-sm bg-gray-100 mt-12 mb-12"
+                    placeholder="Password"
+                    type='password' 
+                    name='password' 
+                    value={this.state.password} 
+                    onChange={this.handleChange} 
+                    required />
+                    
+                    <button 
+                    type='submit'
+                    className="uppercase block mt-12 mb-12 w-full p-4 text-lg rounded-full bg-rose-400 hover:bg-indigo-600 focus:outline-none">
+                      Login
+                    </button>
                 </form>
                 <p className='error-message'>&nbsp;{this.state.error}</p>
             </div>
