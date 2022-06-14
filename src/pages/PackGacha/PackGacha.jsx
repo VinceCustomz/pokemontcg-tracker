0e8d086c995b10a1a01abd859c72034607b2a0dc
vincecustomz <vince.vuong@live.com>
2022-06-14 18:53:21 -0400

    GACHA WORKS OH MY GOD

--- src/pages/PackGacha/PackGacha.jsx
+++ src/pages/PackGacha/PackGacha.jsx
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 
 export default class PackGacha extends Component {
   state = {
+    card0:
+      "https://i5.walmartimages.com/asr/0a32d419-d745-45c0-b367-927f43a0b9fb.bd571c5c075cece9f4893f1cf10926c4.jpeg?odnHeight=450&odnWidth=450&odnBg=ffffff",
     card1:
       "https://i5.walmartimages.com/asr/0a32d419-d745-45c0-b367-927f43a0b9fb.bd571c5c075cece9f4893f1cf10926c4.jpeg?odnHeight=450&odnWidth=450&odnBg=ffffff",
     card2:
@@ -20,25 +22,38 @@ export default class PackGacha extends Component {
       "https://i5.walmartimages.com/asr/0a32d419-d745-45c0-b367-927f43a0b9fb.bd571c5c075cece9f4893f1cf10926c4.jpeg?odnHeight=450&odnWidth=450&odnBg=ffffff",
     card9:
       "https://i5.walmartimages.com/asr/0a32d419-d745-45c0-b367-927f43a0b9fb.bd571c5c075cece9f4893f1cf10926c4.jpeg?odnHeight=450&odnWidth=450&odnBg=ffffff",
-    card10:
-      "https://i5.walmartimages.com/asr/0a32d419-d745-45c0-b367-927f43a0b9fb.bd571c5c075cece9f4893f1cf10926c4.jpeg?odnHeight=450&odnWidth=450&odnBg=ffffff",
   };
 
   handleGacha = async (e) => {
     e.preventDefault();
-    this.setState({ card1: "" });
+    // this.setState({ card1: "" });
     try {
-      let card1 = this.state.card1;
+      let tempCard1 = this.state.card1;
       const jwt = localStorage.getItem("token");
       //GET request, it will go to server.js > go through app.use('/api/cards') > routes > gacha > router.get('/', gachaCtrl.index) and trigger the INDEX controller in controllers/api/gacha
+      
       const cardDataFull = await fetch("/api/gacha", {
         headers: {
           "Content-Type": "application/json",
           Authorization: "Bearer " + jwt,
         },
-      }).then((res) => res.json());
-
-      this.setState({ card1: card1.image_small });
+      })
+      .then((res) => res.json());
+      
+      console.log(cardDataFull)
+  
+      this.setState({
+        card0: cardDataFull[0].image_small,
+        card1: cardDataFull[1].image_small,
+        card2: cardDataFull[2].image_small,
+        card3: cardDataFull[3].image_small,
+        card4: cardDataFull[4].image_small,
+        card5: cardDataFull[5].image_small,
+        card6: cardDataFull[6].image_small,
+        card7: cardDataFull[7].image_small,
+        card8: cardDataFull[8].image_small,
+        card9: cardDataFull[9].image_small
+      });
     } catch (err) {
       console.error("Error: ", err);
     }
@@ -49,13 +64,11 @@ export default class PackGacha extends Component {
       <div>
         <h1>PackGacha</h1>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 m-2 px-4 rounded" onClick={this.handleGacha}>
-          Flip Cards
-        </button>
-        <button className="bg-blue-500 hover:bg-orange-700 text-white font-bold py-2 m-2 px-4 rounded">
-          Retry
+          FLIP CARDS
         </button>
 
         <div class="grid grid-cols-5 grid-rows-2 gap-y-5 m-3">
+          <img src={this.state.card0}></img>
           <img src={this.state.card1}></img>
           <img src={this.state.card2}></img>
           <img src={this.state.card3}></img>
@@ -65,7 +78,6 @@ export default class PackGacha extends Component {
           <img src={this.state.card7}></img>
           <img src={this.state.card8}></img>
           <img src={this.state.card9}></img>
-          <img src={this.state.card10}></img>
         </div>
       </div>
     );
